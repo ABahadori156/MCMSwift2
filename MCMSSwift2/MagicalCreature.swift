1b3a4903a093dcc066243a8079f1ff80b69054be
Amir Bahadori <pbahadori156@gmail.com>
2016-06-11 13:09:26 -0500

    Step 1 done

---
+++ MCMSSwift2/MagicalCreature.swift
@@ -0,0 +1,18 @@
+//
+//  Creature.swift
+//  MCMSSwift2
+//
+//  Created by Pasha Bahadori on 6/11/16.
+//  Copyright © 2016 Pelican Inc. All rights reserved.
+//
+
+import UIKit
+
+class MagicalCreature: NSObject {
+    let name: String?
+    
+    init(withName name: String) {
+        self.name = name
+    }
+    
+}
--- MCMSSwift2/ViewController.swift
+++ MCMSSwift2/ViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var catNameTextField: UITextField!
+    @IBOutlet weak var personalityTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    var magicalCreatures = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let amir = MagicalCreature(withName: "Amir")
+        let salar = MagicalCreature(withName: "Salar")
+        let mohammed = MagicalCreature(withName: "Mohammed")
+        let vala = MagicalCreature(withName: "Vala")
+        
+        magicalCreatures = [amir, salar, mohammed, vala]
+        
     }
+    
+    
+    
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// ------------------------------------TABLEVIEW METHODS -----------------------------------------
+    func  tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return magicalCreatures.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellID")
+        // as! says that this is a Magical Creature, if it's anything else, xcode will crash.
+        let magicalCreature = self.magicalCreatures[indexPath.row] as! MagicalCreature
+        cell!.textLabel!.text = magicalCreature.name
+        return cell!
+        
     }
 
 
